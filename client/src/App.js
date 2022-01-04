bad6be7e34f7e6d10fcc22760c65f2f1b7e9033e
ferwin <ferwin.arias@gmail.com>
2022-01-03 23:02:24 -0500

    se termina form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form'
 
 function App() {
   return (
     <div className='app'>
-      
+      <Form />
     </div>
   );
 }
