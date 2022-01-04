8e4d125d1425c110ec1eca276a92dc625b841807
ferwin <ferwin.arias@gmail.com>
2022-01-04 12:32:55 -0500

    se crean handles para los imputs

--- client/src/components/form.jsx
+++ client/src/components/form.jsx
@@ -5,27 +5,39 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import Stack from "@mui/material/Stack";
+import "./Form.css";
 
 export default function BasicSelect() {
-  const [type, setType] = React.useState("");
   const [user, setUser] = React.useState({
-      nombre: "",
-      apellido: "",
-      tipo_documento: "",
-      documento: 0,
-      email: "",
-      celular: "",
-      fecha_nacimiento: "",
-      archivo: ""
-  })
+    nombre: "",
+    apellido: "",
+    tipo_documento: "",
+    documento: 0,
+    email: "",
+    celular: "",
+    fecha_nacimiento: "",
+    archivo: "",
+  });
 
-  const handleChange = (event) => {
-    setType(event.target.value);
-  };
+  const handleChange = (e) => {
+    e.preventDefault();
+    setUser({
+      ...user,
+      [e.target.name]:e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+        
+  }
 
   return (
     <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
+      <FormControl fullWidth onSubmit={handleSubmit}>
         <Box
           component="form"
           sx={{
@@ -40,48 +52,96 @@ export default function BasicSelect() {
               id="outlined-required"
               label="nombre"
               placeholder="ingrese su nombre"
+              value={user.nombre}
+              onChange={handleChange}
             />
             <TextField
               required
               id="outlined-required"
               label="apellido"
               placeholder="ingrese su apellido"
+              value={user.apellido}
+              onChange={handleChange}
             />
-              <Box>
-                <InputLabel id="demo-simple-select-label">
-                  Tipo de documento
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={type}
-                  label="Tipo_documento"
-                  onChange={handleChange}
-                >
-                  <MenuItem value="CC">CC</MenuItem>
-                  <MenuItem value="CE">CE</MenuItem>
-                  <MenuItem value="TI">TI</MenuItem>
-                  <MenuItem value="PASAPORTE">PASAPORTE</MenuItem>
-                </Select>
-              </Box>
             <TextField
+              required
               id="outlined-number"
               label="Documento"
               type="number"
               placeholder="ingrese su documento"
+              value={user.documento}
+              onChange={handleChange}
             />
             <TextField
+              required
               id="outlined-number"
               label="celular"
               type="text"
               placholder="ingrese su celular"
+              value={user.celular}
+              onChange={handleChange}
             />
             <TextField
-              id="outlined-number"
-              label="Number"
+              required
+              id="outlined-required"
               type="date"
               placholder="ingrese su fecha de nacimiento"
+              value={user.date}
+              onChange={handleChange}
             />
+          </div>
+          <div className="select">
+            <Box
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "25ch" },
+              }}
+            >
+              <InputLabel id="demo-simple-select-label">
+                Tipo de documento
+              </InputLabel>
+              <Select
+                required
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={user.tipo_documento}
+                label="Tipo_documento"
+                onChange={handleChange}
+              >
+                <MenuItem value="CC">CC</MenuItem>
+                <MenuItem value="CE">CE</MenuItem>
+                <MenuItem value="TI">TI</MenuItem>
+                <MenuItem value="PASAPORTE">PASAPORTE</MenuItem>
+              </Select>
+            </Box>
+          </div>
+          <div>
+            <input
+              accept="image/*"
+              name="archivo"
+              className="upload"
+              style={{ display: "none" }}
+              id="raised-button-file"
+              multiple
+              value={user.archivo}
+              onChange={handleChange}
+              type="file"
+            />
+            <label htmlFor="raised-button-file">
+              <Button
+                variant="raised"
+                component="span"
+                className="btn_upload"
+              >
+                Upload
+              </Button>
+            </label>
+          </div>
+          <div>
+            <Stack direction="row" spacing={2}>
+              <Button label="submit" type="submit" variant="contained" endIcon={<SendIcon />}>
+                Send
+              </Button>
+            </Stack>
           </div>
         </Box>
       </FormControl>
